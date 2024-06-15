8ef525798370fae616ff6131b04f20bd2ece3195
Everton de Aquino Peres <72323813+aquinoefomm@users.noreply.github.com>
2024-06-15 20:47:55 -0300

    Comments added

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+// Database configuration. DB_PASSWORD to be set on environmental variables
 const db = new pg.Client({
-    user: "postgres",
+    user: USER_ID,
     host: "localhost",
     database: "it-career",
-    password: "your-db-password",
+    password: USER_KEY,
     port: 5432,
 });
 
 const app = express();
 const port = 3000;
 
+// Conncetion to DB
 db.connect();
 
+// Query database to fetch records
 let courses = {};
 db.query("SELECT * FROM college_courses", (err, res) => {
     if (err) {
@@ -24,12 +27,15 @@ db.query("SELECT * FROM college_courses", (err, res) => {
     }
 });
 
+// Middlewares 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Set engine
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+// Home page. Query to update records when home page is rendered. 
 app.get('/', (req, res) => {
   db.query("SELECT * FROM college_courses", (err, res) => {
     if (err) {
@@ -41,32 +47,36 @@ app.get('/', (req, res) => {
     res.render('index', { name: "Éverton Peres" });
 });
 
+// Records page get.
 app.get('/registros', (req, res) => {
     res.render('registros', { data: courses });
 });
 
+// Render form for creating new record.
 app.get('/cadastro', (req, res) => {
     res.render('form-cadastro', { name: "Éverton Peres" });
 });
 
-app.post('/inserir', async (req, res) => {
+// Create new record. Post from form with new info and include on DB.
+app.post('/inserir', (req, res) => {
     let codigo = req.body.codigo;
     let nome = req.body.nome;
     let carga = req.body.carga;
     let professor = req.body.professor;
     console.log(codigo, nome, carga, professor)
     const values = [codigo, nome, carga, professor]
-    await db.query(`INSERT INTO college_courses(codigod, nomed, cargad, professor) VALUES($1,$2,$3,$4) ON CONFLICT(codigod) DO NOTHING`, values);
+    db.query(`INSERT INTO college_courses(codigod, nomed, cargad, professor) VALUES($1,$2,$3,$4) ON CONFLICT(codigod) DO NOTHING`, values);
     db.query('COMMIT');
     res.redirect('/');
 });
 
-app.post('/editar', async (req, res) => {
+// Render form with data from selected record. 
+app.post('/editar', (req, res) => {
     let codigod = req.body.codigod;
     let nomed = req.body.nomed;
     let cargad = req.body.cargad;
     let professord = req.body.professord;
-    await res.render('edit', { 
+    res.render('edit', { 
     codigo: codigod,
     nome: nomed,
     carga: cargad,
@@ -74,6 +84,7 @@ app.post('/editar', async (req, res) => {
   })
 });
 
+// Update record. Post from form with updated info and update on DB.
 app.post('/update', async (req, res) => {
     let cod = Number(req.body.codigo);
     let nome = req.body.nome;
@@ -85,6 +96,7 @@ app.post('/update', async (req, res) => {
     res.redirect('/');
 })
 
+// Delete record.
 app.post('/delete', (req, res) => {
   let cod = Number(req.body.codigod);
   db.query(`DELETE FROM college_courses WHERE codigod = ${cod}`);
@@ -92,10 +104,7 @@ app.post('/delete', (req, res) => {
   res.redirect('/');
 })
 
-app.post('/editar', (req, res) => {
-  res.redirect('/');
-})
-
+// Start server.
 app.listen(port, (req, res) => {
     console.log(`Server started on port ${port}`);
 });
